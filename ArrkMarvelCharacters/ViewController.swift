6ca78cccdadcea26302e4616033ba246406e1c88
Priyanka Mistry <priyankamistry@priyankas-macbook-pro.local>
2018-06-18 10:47:02 +0530

    Initial Commit

---
+++ ArrkMarvelCharacters/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ArrkMarvelCharacters
+//
+//  Created by Priyanka Mistry on 18/06/18.
+//  Copyright © 2018 Priyanka Mistry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
